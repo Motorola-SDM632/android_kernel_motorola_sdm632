3395b1fb77fc57485351c4bf37b192d50d65d155
SH4ND <miqueiasantos20@gmail.com>
2024-02-26 10:14:04 -0300

    build.sh: Update script and import anykernel
    Change-Id: Iae1b1b033c2844af19957412a81c6015ed26673c

--- build.sh
+++ build.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 #
-TC_DIR="$(pwd)/tc/clang-r487747c"
+# Compile script for Supra kernel
+# Copyright (C) 2020-2021 Adithya R.
+
+SECONDS=0 # builtin bash timer
+ZIPNAME="Kerneldirty-KSU-ocean-$(date '+%Y%m%d-%H%M').zip"
+TC_DIR="$(pwd)/tc/clang-r450784e"
+AK3_DIR="$(pwd)/android/AnyKernel3"
 DEFCONFIG="ocean_defconfig"
+
+if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
+   head=$(git rev-parse --verify HEAD 2>/dev/null); then
+	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
+fi
 
 export PATH="$TC_DIR/bin:$PATH"
 
 if ! [ -d "$TC_DIR" ]; then
-  echo "AOSP clang not found! Cloning to $TC_DIR..."
-  if ! git clone --depth=1 -b 17 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone "$TC_DIR"; then
-    echo "Cloning failed! Aborting..."
-    exit 1
-  fi
+	echo "AOSP clang não encontrando ! Clonando para $TC_DIR..."
+	if ! git clone --depth=1 -b 17 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone "$TC_DIR"; then
+		echo "Clone falhou! Abortando..."
+		exit 1
+	fi
 fi
 
 if [[ $1 = "-c" || $1 = "--clean" ]]; then
-  rm -rf out
+	rm -rf out
 fi
 
 # KSU
@@ -27,3 +38,25 @@ echo -e "\nStarting compilation...\n"
 make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 Image.gz
 
 kernel="out/arch/arm64/boot/Image.gz"
+
+if [ -f "$kernel" ]; then
+	echo -e "\nKernel compilado com sucesso! Compactando...\n"
+	if [ -d "$AK3_DIR" ]; then
+		cp -r $AK3_DIR AnyKernel3
+	elif ! git clone -q https://github.com/Motorola-SDM632/AnyKernel3 -b ocean; then
+		echo -e "\nAnyKernel3 repositorio não encontrado localmente e não foi possível clonar do GitHub! Abortando..."
+		exit 1
+	fi
+	cp $kernel AnyKernel3
+	rm -rf out/arch/arm64/boot
+	cd AnyKernel3
+	git checkout ocean &> /dev/null
+	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+	cd ..
+	rm -rf AnyKernel3
+	echo -e "\nCompletado em $((SECONDS / 60)) minuto(s) and $((SECONDS % 60)) segundo(s) !"
+	echo "Zip: $ZIPNAME"
+else
+	echo -e "\nCompilação falhou!"
+	exit 1
+fi
